1a154772cf6d7ac10d784d87a4cc862f780074ae
Androbin <robin.richtsfeld@gmail.com>
2017-05-18 20:55:08 +0200

    updated formatting

--- src/de/androbin/util/PrintUtil.java
+++ src/de/androbin/util/PrintUtil.java
@@ -6,49 +6,45 @@ import java.awt.image.*;
 import java.util.function.*;
 import javax.swing.*;
 
-public final class PrintUtil
-{
-	private PrintUtil()
-	{
-	}
-	
-	public static void print( final BufferedImage image, final String title )
-	{
-		print( ( job, g ) ->
-		{
-			final int res = job.getPageResolution();
-			final Dimension dim = job.getPageDimension();
-			
-			final float rand = res / 1.27f;
-			
-			final float w = dim.width - rand * 2f;
-			final float h = dim.height - rand * 2f;
-			
-			final boolean format = h * image.getWidth() < w * image.getHeight();
-			
-			final float width = format ? h * image.getWidth() / image.getHeight() : w;
-			final float height = format ? h : w * image.getHeight() / image.getWidth();
-			
-			drawImage( g, image, ( dim.width - width ) * 0.5f, ( dim.height - height ) * 0.5f, width, height );
-		}, title );
-	}
-	
-	public static void print( final BiConsumer<PrintJob, Graphics> renderer, final String title )
-	{
-		new Thread( () ->
-		{
-			final JFrame window = new JFrame();
-			final PrintJob job = Toolkit.getDefaultToolkit().getPrintJob( window, title, null );
-			
-			if ( job != null )
-			{
-				final Graphics g = job.getGraphics();
-				renderer.accept( job, g );
-				g.dispose();
-				job.end();
-			}
-			
-			window.dispose();
-		}, "Printer" ).start();
-	}
+public final class PrintUtil {
+  private PrintUtil() {
+  }
+  
+  public static void print( final BufferedImage image, final String title ) {
+    print( ( job, g ) -> {
+      final int res = job.getPageResolution();
+      final Dimension dim = job.getPageDimension();
+      
+      final float rand = res / 1.27f;
+      
+      final float w = dim.width - rand * 2f;
+      final float h = dim.height - rand * 2f;
+      
+      final boolean format = h * image.getWidth() < w * image.getHeight();
+      
+      final float width = format ? h * image.getWidth() / image.getHeight() : w;
+      final float height = format ? h : w * image.getHeight() / image.getWidth();
+      
+      final float x = ( dim.width - width ) * 0.5f;
+      final float y = ( dim.height - height ) * 0.5f;
+      
+      drawImage( g, image, x, y, width, height );
+    }, title );
+  }
+  
+  public static void print( final BiConsumer<PrintJob, Graphics> renderer, final String title ) {
+    new Thread( () -> {
+      final JFrame window = new JFrame();
+      final PrintJob job = Toolkit.getDefaultToolkit().getPrintJob( window, title, null );
+      
+      if ( job != null ) {
+        final Graphics g = job.getGraphics();
+        renderer.accept( job, g );
+        g.dispose();
+        job.end();
+      }
+      
+      window.dispose();
+    }, "Printer" ).start();
+  }
 }
